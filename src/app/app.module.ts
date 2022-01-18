da6ef647c1512d99dbfdd168a4ed80bdfeeec598
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2022-01-18 22:36:28 +0600

    About HttpClientModule and Angular HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -25,6 +25,7 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '404', component : NotFoundComponent},
